e4a3d45aab7c5738a038dae0085f74cdbe4fbb5b
Ron Forbes <ronforbes528@gmail.com>
2018-10-14 21:53:37 -0700

    Updated SignIn, SignUp and Title scenes: updated UI and added scene navigation

---
+++ Assets/Scripts/SceneNavigator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigator : MonoBehaviour {
+	public void Navigate(string sceneName) {
+		StartCoroutine(LoadSceneAsync(sceneName));
+	}
+
+	IEnumerator LoadSceneAsync(string sceneName) {
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+		while(!asyncLoad.isDone) {
+			yield return null;
+		}
+	}
+}
