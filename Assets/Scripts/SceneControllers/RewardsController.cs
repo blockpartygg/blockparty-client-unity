248a9f7ad1d5df131cf2a93a83c06938071d8992
Ron Forbes <ronforbes528@gmail.com>
2018-10-23 15:08:58 -0700

    Updated RewardsController: added ad watching

--- Assets/Scripts/SceneControllers/RewardsController.cs
+++ Assets/Scripts/SceneControllers/RewardsController.cs
@@ -1,10 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
 public class RewardsController : MonoBehaviour {
 	public void WatchAd() {
-		Debug.Log("Watch ad");
+		if(Advertisement.IsReady("rewardedVideo")) {
+			ShowOptions options = new ShowOptions { resultCallback = HandleShowResult };
+			Advertisement.Show("rewardedVideo", options);
+		}
+	}
+
+	void HandleShowResult(ShowResult result) {
+		switch(result) {
+			case ShowResult.Finished:
+				Debug.Log("Dispense reward");
+				break;
+			case ShowResult.Skipped:
+				Debug.Log("Ad was skipped");
+				break;
+			case ShowResult.Failed:
+				Debug.Log("Ad failed to show");
+				break;
+		}
 	}
 
 	public void PurchaseSkin() {
