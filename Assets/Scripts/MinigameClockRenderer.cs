e98f90928463045c5ae8fbca38ef48130abcd811
Ron Forbes <ronforbes528@gmail.com>
2018-10-30 14:44:26 -0700

    Implemented in-game clock display

--- Assets/Scripts/MinigameClockRenderer.cs
+++ Assets/Scripts/MinigameClockRenderer.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class MinigameClockRenderer : MonoBehaviour {
 	public TMP_Text ClockText;
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update() {
+		switch(GameManager.Instance.State) {
+			case GameManager.GameState.MinigameStart:
+				ClockText.text = "30<sup>:00</sup>";
+				break;
+			case GameManager.GameState.MinigamePlay:
+				TimeSpan timeRemaining = GameManager.Instance.EndTime > DateTime.Now ? GameManager.Instance.EndTime - DateTime.Now : TimeSpan.Zero;
+				ClockText.text = string.Format("{0:D2}<sup>:{1:D3}</sup>", timeRemaining.Seconds, timeRemaining.Milliseconds);
+				break;
+			case GameManager.GameState.MinigameEnd:
+				ClockText.text = "00<sup>:00</sup>";
+				break;
+		}
 	}
 }
