e1eace88e86b5be6e4ab784d8b95331364456821
Ron Forbes <ron@ronforbes.me>
2018-11-15 20:02:54 -0800

    Properly rendering particle systems based on board raising

--- Assets/Scripts/Gameplay/BlockParty/ParticleManager.cs
+++ Assets/Scripts/Gameplay/BlockParty/ParticleManager.cs
@@ -4,7 +4,10 @@ public class ParticleManager : MonoBehaviour {
     public GameObject[,] Particles;
     public GameObject ParticlePrefab;
     public GameObject ParticleParent;
+    public BoardRaiser BoardRaiser;
     public const int Columns = 6, Rows = 12;
+
+	Vector3 initialPosition;
 
     void Awake() {
         Particles = new GameObject[Columns, Rows];
@@ -16,5 +19,15 @@ public class ParticleManager : MonoBehaviour {
                 Particles[column, row].transform.SetParent(ParticleParent.transform, false);
             }
         }
+    }
+
+    void Start() {
+        initialPosition = transform.position;
+    }
+
+    void Update() {
+        Vector3 raiseTranslation = initialPosition + new Vector3(0, BoardRaiser.Elapsed / BoardRaiser.Duration);
+
+		transform.position = raiseTranslation;
     }
 }
