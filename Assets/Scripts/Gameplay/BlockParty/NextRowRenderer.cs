15b53a14faaeb4260cb0e36d359c7722040509e6
Ron Forbes <ron@ronforbes.me>
2018-11-15 20:02:11 -0800

    Properly displaying the next row of blocks darkened with proper raising mechanics

---
+++ Assets/Scripts/Gameplay/BlockParty/NextRowRenderer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class NextRowRenderer : MonoBehaviour {
+	public BoardRaiser BoardRaiser;
+	Vector3 initialPosition;
+
+	void Start() {
+		initialPosition = transform.position;
+	}
+
+	void Update() {
+		Vector3 raiseTranslation = initialPosition + new Vector3(0, BoardRaiser.Elapsed / BoardRaiser.Duration);
+
+		transform.position = raiseTranslation;
+	}
+}
