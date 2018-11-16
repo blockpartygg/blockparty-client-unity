0262cfed69689d80002b6ac4680e09fda820ee7c
Ron Forbes <ron@ronforbes.me>
2018-11-16 11:22:00 -0800

    Fixed rendering bug for next block row rendering

--- Assets/Scripts/Gameplay/BlockParty/NextRowRenderer.cs
+++ Assets/Scripts/Gameplay/BlockParty/NextRowRenderer.cs
@@ -2,15 +2,9 @@
 
 public class NextRowRenderer : MonoBehaviour {
 	public BoardRaiser BoardRaiser;
-	Vector3 initialPosition;
-
-	void Start() {
-		initialPosition = transform.position;
-	}
 
 	void Update() {
-		Vector3 raiseTranslation = initialPosition + new Vector3(0, BoardRaiser.Elapsed / BoardRaiser.Duration);
-
-		transform.position = raiseTranslation;
+		Vector3 raiseTranslation = new Vector3(0, BoardRaiser.Elapsed / BoardRaiser.Duration);
+		transform.position = transform.parent.position + raiseTranslation;
 	}
 }
